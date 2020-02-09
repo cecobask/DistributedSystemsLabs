611f553b1a2e029c926f51227a810b0354b8f66e
Tsvetoslav Dimov <baskski@gmail.com>
2020-02-09 15:27:33 +0000

    Added option to update a db record and changed db queries to use PreparedStatement, instead of Statement. This reduces the risk of SQL injection attack.

--- DBDemo/src/main/java/GUIDemo.java
+++ DBDemo/src/main/java/GUIDemo.java
@@ -20,8 +20,8 @@ public class GUIDemo implements ActionListener, DocumentListener {
     private JButton clearBtn;
     private JButton deleteBtn;
     private JButton addBtn;
+    private JButton updateBtn;
     private ResultSet data;
-    private Dotenv dotenv;
     private Connection conn;
     private boolean tableIsEmpty;
 
@@ -42,6 +42,18 @@ public class GUIDemo implements ActionListener, DocumentListener {
         clearBtn = new JButton("Clear");
         deleteBtn = new JButton("Delete");
         addBtn = new JButton("Add");
+        updateBtn = new JButton("Update");
+
+        // Create an ArrayList of components and them to the panel.
+        addComponents(
+                panel,
+                new JComponent[]{idLbl, idTxt, fNameLbl, fNameTxt, lNameLbl, lNameTxt, emailLbl, emailTxt,
+                        prevBtn, nextBtn, clearBtn, deleteBtn, addBtn, updateBtn}
+        );
+
+        frame.add(panel);
+        frame.setVisible(true);
+        frame.pack();
 
         // Action listeners.
         nextBtn.addActionListener(this);
@@ -49,82 +61,74 @@ public class GUIDemo implements ActionListener, DocumentListener {
         clearBtn.addActionListener(this);
         deleteBtn.addActionListener(this);
         addBtn.addActionListener(this);
+        updateBtn.addActionListener(this);
 
         // Document listeners.
         idTxt.getDocument().addDocumentListener(this);
 
         // Loads the environment variables from .env file.
-        dotenv = Dotenv.load();
+        Dotenv dotenv = Dotenv.load();
 
         try {
-            // Load the data from specified table.
-            data = loadData(
-                    dotenv.get("DB_TEST"),
-                    dotenv.get("TABLE_NAME"),
+            // Establish connection to the database table.
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + dotenv.get("DB_TEST"),
                     dotenv.get("USERNAME"),
                     dotenv.get("PASSWORD")
             );
-            // Create an ArrayList of components and them to the panel.
-            addComponents(
-                    panel,
-                    new JComponent[]{idLbl, idTxt, fNameLbl, fNameTxt, lNameLbl, lNameTxt, emailLbl, emailTxt,
-                            prevBtn, nextBtn, clearBtn, deleteBtn, addBtn}
-            );
 
-            frame.add(panel);
-            frame.setVisible(true);
-            frame.pack();
-
-            // Display the loaded data in the widgets.
+            // Load and display the data.
+            data = loadData();
             displayData();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    private ResultSet loadData(String dbName, String tableName, String userName, String password) throws SQLException {
-        conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + dbName,
-                userName,
-                password
-        );
-        Statement st = conn.createStatement();
-        ResultSet rs = st.executeQuery("SELECT * FROM " + tableName);
+    private ResultSet loadData() {
+        try {
+            PreparedStatement st = conn.prepareStatement("SELECT * FROM data");
+            ResultSet rs = st.executeQuery();
 
-        // Check if the table is empty.
-        if (!rs.next()) {
-            // Disable buttons.
-            deleteBtn.setEnabled(false);
+            // Check if the table is empty.
+            if (!rs.next()) {
+                // Disable buttons.
+                deleteBtn.setEnabled(false);
+                prevBtn.setEnabled(false);
+                nextBtn.setEnabled(false);
+                // Clear input boxes.
+                clearBtn.doClick();
+                showMessageDialog("Table 'data' is empty!");
+                tableIsEmpty = true;
+                return null;
+            }
+
+            // Count the number of rows in the table.
+            int count = 0;
+            rs.beforeFirst(); // Point cursor at default position.
+            while (rs.next()) {
+                count++;
+            }
+
+            // Disable 'Next' button if there are 0 or 1 rows in the table.
+            if (count <= 1) {
+                nextBtn.setEnabled(false);
+            } else {
+                nextBtn.setEnabled(true);
+            }
+
+            // Point cursor at first row and disable 'Previous' button by default.
+            rs.first();
             prevBtn.setEnabled(false);
-            nextBtn.setEnabled(false);
-            // Clear input boxes.
-            clearBtn.doClick();
-            showMessageDialog("Table '" + dotenv.get("TABLE_NAME") + "' is empty!");
-            tableIsEmpty = true;
+            deleteBtn.setEnabled(true);
+
+            tableIsEmpty = false;
+
+            return rs;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            showMessageDialog("There was a problem loading the data! Check the logs for more info.");
             return null;
         }
-
-        // Count the number of rows in the table.
-        int count = 0;
-        rs.beforeFirst(); // Point cursor at default position.
-        while (rs.next()) {
-            count++;
-        }
-
-        // Disable 'Next' button if there are 0 or 1 rows in the table.
-        if (count <= 1) {
-            nextBtn.setEnabled(false);
-        } else {
-            nextBtn.setEnabled(true);
-        }
-
-        // Point cursor at first row and disable 'Previous' button by default.
-        rs.first();
-        prevBtn.setEnabled(false);
-        deleteBtn.setEnabled(true);
-
-        tableIsEmpty = false;
-
-        return rs;
     }
 
     private void addComponents(JComponent panel, JComponent[] components) {
@@ -157,11 +161,12 @@ public class GUIDemo implements ActionListener, DocumentListener {
         );
     }
 
-    private void deleteRecord(String table, int id) {
+    private void deleteRecord(int id) {
         try {
             // Execute DELETE query.
-            Statement st = conn.createStatement();
-            int result = st.executeUpdate("DELETE FROM " + table + " WHERE id=" + id);
+            PreparedStatement st = conn.prepareStatement("DELETE FROM data WHERE id=?");
+            st.setInt(1, id);
+            int result = st.executeUpdate();
 
             if (result == 0) {
                 showMessageDialog("Record with ID '" + id + "' not found!");
@@ -169,43 +174,58 @@ public class GUIDemo implements ActionListener, DocumentListener {
             }
 
             // Show what was deleted.
-            showMessageDialog("Deleted user with ID: " + idTxt.getText());
+            showMessageDialog("Deleted record with ID: " + id);
 
             // Reload the data.
-            data = loadData(
-                    dotenv.get("DB_TEST"),
-                    dotenv.get("TABLE_NAME"),
-                    dotenv.get("USERNAME"),
-                    dotenv.get("PASSWORD")
-            );
+            data = loadData();
             displayData();
         } catch (SQLException e) {
             e.printStackTrace();
-            showMessageDialog("There was a problem deleting this record.");
+            showMessageDialog("There was a problem deleting this record! Check the logs for more info.");
         }
     }
 
-    private void addRecord(String table, int id, String name, String lastname, String email) {
+    private void addRecord(int id, String name, String lastname, String email) {
         try {
-            // Execute DELETE query.
-            Statement st = conn.createStatement();
-            st.executeUpdate(
-                    "INSERT INTO " + table + " VALUES (" + id + ", '" + name + "', '" + lastname + "', '" + email + "')"
+            // Execute INSERT query.
+            PreparedStatement st = conn.prepareStatement(
+                    "INSERT INTO data (id, name, lastname, email) VALUES (?, ?, ?, ?)"
             );
+            st.setInt(1, id);
+            st.setString(2, name);
+            st.setString(3, lastname);
+            st.setString(4, email);
+            st.executeUpdate();
 
-            showMessageDialog("Successfully inserted new record into table '" + dotenv.get("TABLE_NAME") + "'");
+            showMessageDialog("Inserted record with ID: " + id);
 
             // Reload the data.
-            data = loadData(
-                    dotenv.get("DB_TEST"),
-                    dotenv.get("TABLE_NAME"),
-                    dotenv.get("USERNAME"),
-                    dotenv.get("PASSWORD")
-            );
+            data = loadData();
             displayData();
         } catch (SQLException e) {
             e.printStackTrace();
-            showMessageDialog("There was a problem inserting this record.");
+            showMessageDialog("There was a problem inserting this record! Check the logs for more info.");
+        }
+    }
+
+    private void updateRecord(int id, String name, String lastname, String email) {
+        try {
+            // Execute UPDATE query.
+            PreparedStatement st = conn.prepareStatement("UPDATE data SET name=?, lastname=?, email=? WHERE id=?");
+            st.setString(1, name);
+            st.setString(2, lastname);
+            st.setString(3, email);
+            st.setInt(4, id);
+            st.executeUpdate();
+
+            showMessageDialog("Updated record with ID: " + id);
+
+            // Reload the data.
+            data = loadData();
+            displayData();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            showMessageDialog("There was a problem updating this record! Check the logs for more info.");
         }
     }
 
@@ -252,8 +272,8 @@ public class GUIDemo implements ActionListener, DocumentListener {
                     case 0:
                         // Check if 'ID' field is of integer format.
                         if (validIdFormat()) {
-                            // Delete current row from the table.
-                            deleteRecord(dotenv.get("TABLE_NAME"), Integer.parseInt(idTxt.getText()));
+                            // Delete record with specified ID.
+                            deleteRecord(Integer.parseInt(idTxt.getText()));
                         }
                         break;
                     // Clicked 'No'.
@@ -265,10 +285,17 @@ public class GUIDemo implements ActionListener, DocumentListener {
             else if (event.getSource() == addBtn) {
                 // Check if 'ID' field is of integer format.
                 if (validIdFormat()) {
-                    // Insert a record into specified table.
-                    addRecord(
-                            dotenv.get("TABLE_NAME"), Integer.parseInt(idTxt.getText()),
-                            fNameTxt.getText(), lNameTxt.getText(), emailTxt.getText()
+                    // Insert a record.
+                    addRecord(Integer.parseInt(idTxt.getText()), fNameTxt.getText(), lNameTxt.getText(), emailTxt.getText());
+                }
+            }
+            // 'Update' button pressed.
+            else if (event.getSource() == updateBtn) {
+                // Check if 'ID' field is of integer format.
+                if (validIdFormat()) {
+                    // Update the specified record.
+                    updateRecord(
+                            Integer.parseInt(idTxt.getText()), fNameTxt.getText(), lNameTxt.getText(), emailTxt.getText()
                     );
                 }
             }
@@ -308,22 +335,25 @@ public class GUIDemo implements ActionListener, DocumentListener {
     public void enableDisableButtons(DocumentEvent e) {
         Document doc = e.getDocument();
         // Text input field is empty.
-        if (doc.getLength() == 0 && !tableIsEmpty) {
+        if (doc.getLength() == 0) {
             clearBtn.setEnabled(false);
             deleteBtn.setEnabled(false);
             addBtn.setEnabled(false);
-        } else if (doc.getLength() == 0 && tableIsEmpty) {
-            clearBtn.setEnabled(false);
-            deleteBtn.setEnabled(false);
-            addBtn.setEnabled(false);
-        } else if (doc.getLength() > 0 && tableIsEmpty) {
+            updateBtn.setEnabled(false);
+        }
+        // Text input field is not empty, but the table is.
+        else if (doc.getLength() > 0 && tableIsEmpty) {
             clearBtn.setEnabled(true);
             deleteBtn.setEnabled(false);
             addBtn.setEnabled(true);
-        } else {
+            updateBtn.setEnabled(false);
+        }
+        // Both text input field and table are not empty.
+        else {
             clearBtn.setEnabled(true);
             deleteBtn.setEnabled(true);
             addBtn.setEnabled(true);
+            updateBtn.setEnabled(true);
         }
     }
 
